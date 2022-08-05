6985c42b6c9df5ee360ae12c5900f11a6694f332
XinleZhou2018 <39542071+XinleZhou2018@users.noreply.github.com>
2022-08-05 14:50:20 +0800

    ...

--- weibo/lib/core/network/api_util.dart
+++ weibo/lib/core/network/api_util.dart
@@ -1,6 +1,6 @@
 /// APP 接口 及 服务器地址配置类
 class ApiUtil {
-  static const DEBUG_MODE = true;
+  static const DEBUG_MODE = false;
   static const LOG_DEBUG_MODE = true; //日志log
 
   static const PROXY_HOST = "8888";
@@ -12,16 +12,6 @@ class ApiUtil {
   static const API = "/weibo";
 
   ///开发环境
-  // static const DEBUG_HOST = "47.111.208.192:8005";
-
-  //本地环境
-  // static const DEBUG_HOST = "172.16.41.200:8005";
-
-  //Beta环境
-  // static const DEBUG_HOST = "47.98.205.31:8005";
-
-  ///测试环境
-  // static const DEBUG_HOST = "192.168.3.176:8088";
 
   static const DEBUG_HOST = "172.16.41.69:8088";
   // static const DEBUG_HOST = "192.168.3.207:8088";
